3e2977288f9846816c673f9120f5d171cd83b560
venti <xorals9448@herren.co.kr>
2022-09-18 15:15:18 +0900

    임포트 수정

--- src/test/kotlin/org/pp/practice/common/AcceptanceTest.java
+++ src/test/kotlin/org/pp/practice/common/AcceptanceTest.java
@@ -1,7 +1,5 @@
 package org.pp.practice.common;
 
-import java.lang.reflect.Type;
-
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,13 +8,7 @@ import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.test.context.ActiveProfiles;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
-import com.fasterxml.jackson.module.kotlin.KotlinModule;
-
 import io.restassured.RestAssured;
-import io.restassured.config.ObjectMapperConfig;
-import io.restassured.mapper.ObjectMapper;
-import io.restassured.mapper.ObjectMapperType;
-import io.restassured.path.json.mapper.factory.Jackson2ObjectMapperFactory;
 
 @Testcontainers
 @ActiveProfiles("test")
